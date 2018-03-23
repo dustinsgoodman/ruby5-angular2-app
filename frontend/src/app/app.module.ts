9e7f22cee58a4391cf657ad656df3af4327f891e
Dustin Goodman <dustin.s.goodman@gmail.com>
2018-03-23 14:19:05 -0500

    scaffold customer-add component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { OrderAddComponent } from './order-add/order-add.component';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductAddComponent,
-    OrderAddComponent
+    OrderAddComponent,
+    CustomerAddComponent
   ],
   imports: [
     BrowserModule
